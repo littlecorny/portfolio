8a69956a051ee3a78e779b2d101c51132f1903f1
littlecorny <carlapajuelopaniagua2.0@gmail.com>
2025-11-11 15:45:48 +0100

    bug fix

--- portfolio-git/src/components/Layout.jsx
+++ portfolio-git/src/components/Layout.jsx
@@ -2,6 +2,7 @@ import { Outlet } from "react-router-dom";
 import Footer from "./Footer";
 import Header from "./Header";
 import Tarjetas from "./TarjetasContenedor";
+import "./Layout.css"
 
             export default function Layout() {
               return (
