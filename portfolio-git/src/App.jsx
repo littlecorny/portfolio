88703c201b9693d82d3aefb9cca31e8f0dec964e
littlecorny <carlapajuelopaniagua2.0@gmail.com>
2025-11-11 14:05:21 +0100

    intento vercel

--- portfolio-git/src/App.jsx
+++ portfolio-git/src/App.jsx
@@ -1,15 +1,15 @@
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
 import Home from "./pages/Home";
 
+ 
 export default function App() {
   return (
-    <Router>
-      <Routes>
-        <Route element={<Layout />}>
-          <Route path="/" element={<Home />} />
-        </Route>
-      </Routes>
-    </Router>
+    <Routes>
+      <Route element={<Layout />}>
+      <Route path="/" element={<Home />} />
+      </Route>
+    </Routes>
   );
 }
+ 
